039320e6ab0e5ac7c668868d367ed7b0057cc5f7
StarkMax <zravenz.max@gmail.com>
2020-12-13 16:08:52 +0700

    1

--- src/components/NavBar/LeftNavBar.jsx
+++ src/components/NavBar/LeftNavBar.jsx
@@ -1,5 +1,5 @@
 import { React } from 'react';
-import { Nav, Navbar} from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import NavBarItem from './NavBarItem';
 
@@ -7,21 +7,21 @@ import NavBarItem from './NavBarItem';
 
 
 const LeftNavBar = (props) => {
+
+  let navBarItem = props.catalog.map(item => {
+    return (
+        <NavBarItem id={item.id} name={item.name}></NavBarItem>
+      );
+    }
+  );
   return (
     <div className="App-leftNavBar">
-        <Navbar bg="light" variant="light">
-          <Nav className="flex-column" style={{padding:20}}>
-            <NavBarItem id={props.catalog[0].id} name={props.catalog[0].name}></NavBarItem>
-            <NavBarItem id={props.catalog[1].id} name={props.catalog[1].name}></NavBarItem>
-            <NavBarItem id={props.catalog[2].id} name={props.catalog[2].name}></NavBarItem>
-            <NavBarItem id={props.catalog[3].id} name={props.catalog[3].name}></NavBarItem>
-            <NavBarItem id={props.catalog[4].id} name={props.catalog[4].name}></NavBarItem>
-            <NavBarItem id={props.catalog[5].id} name={props.catalog[5].name}></NavBarItem>
-            <NavBarItem id={props.catalog[6].id} name={props.catalog[6].name}></NavBarItem>
-            <NavBarItem id={props.catalog[7].id} name={props.catalog[7].name}></NavBarItem>
-            <NavLink to="#">Услуги</NavLink>
-          </Nav>
-        </Navbar>
+      <Navbar bg="light" variant="light">
+        <Nav className="flex-column" style={{ padding: 20 }}>
+          {navBarItem}
+          <NavLink to="#">Услуги</NavLink>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
