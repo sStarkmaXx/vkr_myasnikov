dac3d3f61cab952d8f4679264d7aa157b289ef96
Макс Мясников <zravenz.max@gmail.com>
2020-12-29 21:13:17 +0700

    1

--- src/components/content/itemInfoPage/ItemInfo.jsx
+++ src/components/content/itemInfoPage/ItemInfo.jsx
@@ -1,29 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { React } from 'react';
-import { Form, Card, Button, ListGroup } from 'react-bootstrap';
-import './ItemInfo.module.css';
+import { Form, Button} from 'react-bootstrap';
+import classes from './ItemInfo.module.css';
 
 
 
 const ItemInfo = (props) => {
-  debugger;
+  let url = 'url('+props.selectItem.img+')';
   return (
-    <div className="itemInfo">
-      <img src={props.selectItem.img}/>
-      <Card style={{ width: '18rem' }}>
-        <ListGroup variant="flush">
-          <ListGroup.Item>{props.selectItem.name}</ListGroup.Item>
-          <ListGroup.Item>Цена: {props.selectItem.price} рублей</ListGroup.Item>
-        </ListGroup>
-      </Card>
+    <div className={classes.itemInfo}>
+      <div className={classes.img} style={{backgroundImage:url}}></div>
+      <div className={classes.name}>{props.selectItem.name}</div>
+      <div className={classes.price}>Цена: {props.selectItem.price} рублей</div>
+      <div className={classes.button}><Button variant="info" type="submit">Добавить в корзину</Button></div>
+      <div className={classes.form}>
       <Form>
         <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Оставить коментарий</Form.Label>
+          <Form.Label>Оставить комментарий</Form.Label>
           <Form.Control as="textarea" rows={3} />
         </Form.Group>
         <Button variant="info" type="submit">Отправить</Button>
       </Form>
-      
+      </div>
+      <div className={classes.post}></div>
     </div>
   );
 }
